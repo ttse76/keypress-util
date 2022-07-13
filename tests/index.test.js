4b4069d064d46831151146e69f4352c572612305
Titty Tech <timtse76@gmail.com>
2022-07-13 18:04:29 -0400

    added tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -7,8 +7,21 @@ test('gets valid key', () => {
   expect(getKey(keyCode)).toBe(testKey);
 });
 
+test('gets valid key when number is a string', () => {
+  const keyCode = '36';
+  const testKey = "HOME";
+
+  expect(getKey(keyCode)).toBe(testKey);
+})
+
 test('return "NONE" if key code does not exist', () => {
   const testCode = 500;
+
+  expect(getKey(testCode)).toBe('NONE');
+});
+
+test('return "NONE" if key is not a number', () => {
+  const testCode = 'test key invalid';
 
   expect(getKey(testCode)).toBe('NONE');
 });
